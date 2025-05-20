53b78c66f086d44550a4b89357013249305cf13c
slalom0914 <slalom0914@gmail.com>
2025-05-20 16:30:19 +0900

    회원가입

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/* 회원가입 */
+router.get('/join', function(req, res, next) {
+  res.render('index', { title: '회원가입', pageName: 'pages/users/join.ejs', email: null });
+});
 
 module.exports = router;
