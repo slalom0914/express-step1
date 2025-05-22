574742720eb1f92c7ed2e7eb7e3d50e340893d77
slalom0914 <slalom0914@gmail.com>
2025-05-22 17:18:19 +0900

    유튜브메뉴추가

--- routes/youtube.js
+++ routes/youtube.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Youtube' });
 });
+// 버전1
+router.get('/step1', function(req, res, next){
+  res.render('index', { title: "유튜브-버전1", pageName: "pages/youtube/step1.ejs", email: null })
+})
+// 버전2
+router.get('/step2', function(req, res, next){
+  res.render('index', { title: "유튜브-버전2", pageName: "pages/youtube/step2.ejs", email: null })
+})
+// 버전3
+router.get('/step3', function(req, res, next){
+  res.render('index', { title: "유튜브-버전3", pageName: "pages/youtube/step3.ejs", email: null })
+})
 
 module.exports = router;
