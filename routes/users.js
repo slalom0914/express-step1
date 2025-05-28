fe465ccd1e64b151a1a5280fa7df445cb5e0007c
slalom0914 <slalom0914@gmail.com>
2025-05-28 15:44:33 +0900

    메뉴 추가

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,20 @@ router.get('/', function(req, res, next) {
 router.get('/join', function(req, res, next) {
   res.render('index', { title: '회원가입', pageName: 'pages/users/join.ejs', email: null });
 });
+/* 장바구니 */
+// http://localhost:5000/cart
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: '장바구니', pageName: 'pages/users/cart.ejs', email: null });
+});
+/* MyPage */
+// http://localhost:5000/mypage
+router.get('/mypage', function(req, res, next) {
+  res.render('index', { title: 'MyPage', pageName: 'pages/users/mypage.ejs', email: null });
+});
+/* 회원정보수정 */
+// http://localhost:5000/update
+router.get('/update', function(req, res, next) {
+  res.render('index', { title: '회원정보수정', pageName: 'pages/users/update.ejs', email: null });
+});
 
 module.exports = router;
